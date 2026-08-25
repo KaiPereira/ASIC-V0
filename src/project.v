7e653d67ccfcf2113d9c810f846e8988cf8c37b0
Kai Pereira <kaipereira2020@gmail.com>
2026-08-25 15:41:26 -0400

    mapping out wires

--- src/project.v
+++ src/project.v
@@ -16,10 +16,33 @@ module tt_um_kaipereira_spicontroller (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  wire spi_cs_n; // _n postfixes to denote an active-low signal because the microcontroller wants to communicate when it's low
+  wire spi_clk;
+  wire spi_miso; // Master in, slave out
+  wire spi_mosi; // Master out, slave in
+
+  wire cpol; // Clock polarity
+  wire cpha; // Clock phase
+  wire done; // Whole byte that's been sent to the controller 
+  wire reset; // Reset signal
+
+
+  // Mapping input wires
+  assign spi_cs_n = ui_in[0];
+  assign spi_clk = ui_in[1];
+  assign spi_miso = ui_in[2];
+  assign cpol = ui_in[3];
+  assign cpha = ui_in[4];
+  assign reset = ui_in[5];
+
+  // Mapping output wires
+  assign ui_out[0] = spi_mosi;
+  assign ui_out[1] = done;
+
+
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out = 0; assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
