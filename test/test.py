26b61913982b85872e48e0ea5bf491179863f4c9
Kai Pereira <kaipereira2020@gmail.com>
2026-08-28 14:02:31 -0400

    moving the serial clock to the test.py file instead of testbench

--- test/test.py
+++ test/test.py
@@ -3,15 +3,23 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
 
+
+async def sck_clock(dut, pin, period_ns):
+    while True:
+        dut.uio_in.value = int(dut.uio_in.value) | (1 << pin);
+        await Timer(period_ns / 2, unit="ns");
+        dut.uio_in.value = int(dut.uio_in.value) & ~(1 << pin);
+        await Timer(period_ns / 2, unit="ns");
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 0.0625, unit="us")
+    # Set the clock period to 0.0625 us (16 MHz)
+    clock = Clock(dut.clk, 62.5, unit="ns")
+
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -23,6 +31,8 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
+    cocotb.start_soon(sck_clock(dut, 3, 250));
+
     dut._log.info("Test project behavior")
 
     # Wait for one clock cycle to see the output values
@@ -30,7 +40,7 @@ async def test_project(dut):
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
