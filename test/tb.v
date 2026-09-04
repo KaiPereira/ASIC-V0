4cd3f249497132e408799bfbb121aabbfd53dac1
Kai Pereira <kaipereira2020@gmail.com>
2026-09-04 10:41:07 -0400

    renaming testbench to make the workflow behave as expected :D

--- test/tb.v
+++ test/tb.v
@@ -22,7 +22,7 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  tt_um_kaipereira_spicontroller user_project (
+  tt_um_kaipereira_spi_slave user_project (
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
